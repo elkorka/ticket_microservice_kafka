951c2b2262667ecbb03edb521183bfb49dd33f0d
korka <emkdiallo@gmail.com>
2026-05-19 11:08:56 +0000

    Implementing Api circuit breaker

--- apigateway/src/main/java/com/elz/apigateway/routes/BookingServiceRoutes.java
+++ apigateway/src/main/java/com/elz/apigateway/routes/BookingServiceRoutes.java
@@ -1,10 +1,14 @@
 package com.elz.apigateway.routes;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.*;
+
+import java.net.URI;
 
 @Configuration
 public class BookingServiceRoutes {
@@ -14,6 +18,8 @@ public class BookingServiceRoutes {
         return GatewayRouterFunctions.route("booking-service")
                 .route(RequestPredicates.POST("/api/v1/booking"),
                         HandlerFunctions.http("http://localhost:8081/api/v1/booking"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("bookingServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
                 .build();
     }
 }
